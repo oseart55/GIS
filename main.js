5780bc0c3582dcf99798c7cbb4d32e8173adca5e
Matthew <35896223+oseart55@users.noreply.github.com>
2023-06-18 11:56:50 -0500

    Updated main.js
    Added location to main.js, Lat,Lon.

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ async function loadIntoTable(url, table){
 	const response = await fetch(url);
 	const { fields, features } = await response.json();
 	
-	
+	headers = ["Incident ID", "Date", "Type", "Disposition", "Block Address", "City", "Location"]
 	
 	tableHead.innerHTML = "<tr></tr>";
 	tableBody.innerHTML = "";
 	
-	for (const headerText of fields){
+	for (const headerText of headers){
 		const headerElement = document.createElement("th");
 		
-		headerElement.textContent = headerText.name;
+		headerElement.textContent = headerText;
 		tableHead.querySelector("tr").appendChild(headerElement);
 	}
 	for (const feature of features){
@@ -21,21 +21,28 @@ async function loadIntoTable(url, table){
 		const IncidentId = document.createElement("td");
 		const IncidentDate = document.createElement("td");
 		const Category = document.createElement("td");
+		const Disposition = document.createElement("td");
 		const BlkAddress = document.createElement("td");
 		const City = document.createElement("td");
+		const Location = document.createElement("td");
 		
 		IncidentId.textContent = feature.attributes.IncidentId;
 		var date = new Date(feature.attributes.IncidentDate * 1000)
 		IncidentDate.textContent = date;
 		Category.textContent = feature.attributes.Category;
+		Disposition.textContent = feature.attributes.CadDisposition;
 		BlkAddress.textContent = feature.attributes.BlkAddress;
 		City.textContent = feature.attributes.City;
+		Location.textContent = String(feature.geometry.y) +","+ String(feature.geometry.x)
+		
 		
 		featureElement.appendChild(IncidentId)
 		featureElement.appendChild(IncidentDate)
 		featureElement.appendChild(Category)
+		featureElement.appendChild(Disposition)
 		featureElement.appendChild(BlkAddress)
 		featureElement.appendChild(City)
+		featureElement.appendChild(Location)
 		
 		tableBody.appendChild(featureElement)
 		
@@ -44,5 +51,5 @@ async function loadIntoTable(url, table){
 }
 
 
-loadIntoTable("https://geodata.sarpy.gov/arcgis/rest/services/PublicSafety/PublicCrimeMap/FeatureServer/1/query?where=1%3D1&outFields=IncidentId,IncidentDate,Category,BlkAddress,City&returnGeometry=false&outSR=4326&f=json",document.querySelector("table"));
+loadIntoTable("https://geodata.sarpy.gov/arcgis/rest/services/PublicSafety/PublicCrimeMap/FeatureServer/1/query?where=1%3D1&outFields=IncidentId,IncidentDate,Category,CadDisposition,BlkAddress,City&returnGeometry=true&outSR=4326&f=json",document.querySelector("table"));
 
